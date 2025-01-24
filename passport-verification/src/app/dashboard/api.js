52197407bb92f81cf43985466c769ce1fc9f9a01
SKGHALE <sahebkumarghale@gmail.com>
2025-01-24 12:36:10 +0530

    dashboardCrad

--- passport-verification/src/app/dashboard/api.js
+++ passport-verification/src/app/dashboard/api.js
@@ -5,7 +5,7 @@ import { postRequest } from "../commonAPI";
 export const getDistrictNodalDashBoard = async () => {
   try {
 
-    return await postRequest("user/getDistrictNodalDashBoard");
+    return await postRequest("user/getApplicationCountsV1");
   } catch (error) {
     console.log("Error saving user:", error);
     return null;
