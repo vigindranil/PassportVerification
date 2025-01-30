5dfc32a6f6d77c6764019e65bd37f612b39b429f
akashs-vyoma <akash.s@vyomainnovusglobal.com>
2025-01-30 19:20:39 +0530

    uno

--- passport-verification/src/app/allFiles/api.js
+++ passport-verification/src/app/allFiles/api.js
@@ -6,24 +6,25 @@ export const acceptApplication = async (applicationId, citizentype, file) => {
     const use_details = await fetch("https://ipinfo.io/json");
     const user_details_json = await use_details.json();
 
-    // const macAddress = {
-    //   ip: user_details_json?.ip,
-    //   hostname: user_details_json?.hostname,
-    //   city: user_details_json?.city,
-    //   region: user_details_json?.region,
-    //   country: user_details_json?.country,
-    //   loc: user_details_json?.loc,
-    //   org: user_details_json?.org,
-    //   postal: user_details_json?.postal,
-    //   timezone: user_details_json?.timezone,
-    // };
+    const user_agent = {
+      ip: user_details_json?.ip,
+      hostname: user_details_json?.hostname,
+      city: user_details_json?.city,
+      region: user_details_json?.region,
+      country: user_details_json?.country,
+      loc: user_details_json?.loc,
+      org: user_details_json?.org,
+      postal: user_details_json?.postal,
+      timezone: user_details_json?.timezone,
+    };
+    const jsonTEXT = JSON.stringify(user_agent);
     const locationIp = user_details_json?.ip;
     const deviceId = "-";
     const macAddress = "-";
     return await postFileRequest("upload/acceptCaseUploadDocument", {
       applicationId,
       citizentype,
-      DocTypeId: 13,
+      jsonTEXT,
       file,
       macAddress,
       locationIp,
