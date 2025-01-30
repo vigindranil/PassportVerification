e18d4630149c42b4ad564359cfe2620a4dc1b760
SKGHALE <sahebkumarghale@gmail.com>
2025-01-30 18:47:59 +0530

    uno

---
+++ passport-verification/src/app/completed-oc/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '@/components/navbar';
+import Sidebar from '@/components/sidebar';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import PendingApplicationDatatable from '@/components/pendingApplication';
+
+export default function allFiles() {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-y-auto p-6">
+          <CardContent>
+            <Card>
+              <PendingApplicationDatatable  heading='Completed Verification OC' status={50}/>
+            </Card>
+          </CardContent>
+        </main>
+      </div>
+    </div>
+  )
+}
