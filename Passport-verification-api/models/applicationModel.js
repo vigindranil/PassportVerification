cf97e973440f69184347945be072ce153adf545d
Sayantanikar44 <sayantanikar44@gmail.com>
2025-02-06 15:15:29 +0530

    sayantani

--- Passport-verification-api/models/applicationModel.js
+++ Passport-verification-api/models/applicationModel.js
@@ -125,7 +125,7 @@ export async function updateAADHAARInfoModel(
       `CALL sp_updateAADHAARInfoV1(?,?,?, ?, ?, ?, ?, ?, ?, @ErrorCode)`,
       [
         ApplicationID,
-        AadharNumber,
+        btoa(AadharNumber),
         AadhaarName,
         AadhaarDOB,
         AadharVerifiedStatus,
