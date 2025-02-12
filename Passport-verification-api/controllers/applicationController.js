93b06c02af03a61eab305a86958dc0eac2aaffd0
akashs-vyoma <akash.s@vyomainnovusglobal.com>
2025-02-12 10:48:49 +0530

    akash

--- Passport-verification-api/controllers/applicationController.js
+++ Passport-verification-api/controllers/applicationController.js
@@ -56,7 +56,7 @@ export const getApplicationDetails = async (req, res) => {
     const OperationName = "getApplicationDetails";
     const json = "{}";
 
-    // const saveTransaction = await saveTransactionHistory(ipaddress, macAddress, Longitude, Latitude, 0, OperationName, json, entryUserId)
+    // const saveTransaction = await saveTransactionHistory(ipaddress, macAddress, Longitude, Latitude, applicationId, OperationName, json, entryUserId)
     // console.log(saveTransaction);
 
     logger.debug(
