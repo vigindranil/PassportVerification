838ea509129887ea89e65504ef5a27d16132ff04
anirban <anirban.d@vyomainnovusglobal.com>
2025-12-10 13:52:35 +0530

    done

---
+++ passport-verification/src/app/moreThan15DaysPending/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import DataTable from '@/components/pendingApplication';
+import Image from 'next/image';
+import BackgroundImg from '@/assets/passport-bg.jpg';
+
+export default function allFiles() {
+  return (
+    <div className="flex h-full min-h-[100vh] bg-gray-100 relative">
+      <Image
+                              src={BackgroundImg}
+                              alt="Background Image"
+                              objectFit="cover"
+                              className="absolute opacity-10 inset-0 -z-2"
+                          />
+      <div className="flex-1 flex flex-col overflow-hidden relative">
+        <main className="flex-1 p-6">
+          <CardContent>
+            <Card>
+              <DataTable heading='Verification pending for more than 15 days from the date of receipt' status={75} period={15} last15DaysPending={true} />
+            </Card>
+          </CardContent>
+        </main>
+      </div>
+    </div>
+  )
+}
